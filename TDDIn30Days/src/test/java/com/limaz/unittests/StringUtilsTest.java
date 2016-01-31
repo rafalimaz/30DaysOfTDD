81492e193b8e71a3eefa5e87626185794c1502bc
Rafael de Lima <rafaellima@great.ufc.br>
2016-01-30 21:17:44 -0300

    Day 4: Make Tests to Pass

--- TDDIn30Days/src/test/java/com/limaz/unittests/StringUtilsTest.java
+++ TDDIn30Days/src/test/java/com/limaz/unittests/StringUtilsTest.java
@@ -20,4 +20,17 @@ public class StringUtilsTest
 
         assertEquals(expectedResult, result);
     }
+    
+    @Test
+    public void ShouldBeAbleToCountNumberOfLettersInAComplexSentence()
+    {
+        String sentenceToScan = "Once is unique, twice is a coincidence, three times is a pattern.";
+        String characterToScanFor = "n";
+        int expectedResult = 5;
+        StringUtils stringUtils = new StringUtils();
+
+        int result = stringUtils.FindNumberOfOccurences(sentenceToScan, characterToScanFor);
+
+        assertEquals(expectedResult, result);
+    }
 }
--- TDDIn30Days/src/test/java/com/limaz/utils/StringUtils.java
+++ TDDIn30Days/src/test/java/com/limaz/utils/StringUtils.java
@@ -3,7 +3,14 @@ package com.limaz.utils;
 public class StringUtils {
 
 	public int FindNumberOfOccurences(String sentenceToScan, String characterToScanFor) {
-		return 2;
+		int numberOfOccurrences = 0;
+
+		for (int i = 0; i < sentenceToScan.length(); i++) {
+			if (sentenceToScan.charAt(i) == characterToScanFor.charAt(0)) {
+				numberOfOccurrences++;
+			}
+		}
+		return numberOfOccurrences;
 	}
 
 }
