079479f7684d9ba5af431de7551c777bf8c6ac06
Rafael de Lima <rafaellima@great.ufc.br>
2016-01-30 21:42:59 -0300

    Day 9: Refactoring Basics

--- TDDIn30Days/src/main/java/com/limaz/utils/StringUtils.java
+++ TDDIn30Days/src/main/java/com/limaz/utils/StringUtils.java
@@ -3,17 +3,17 @@ package com.limaz.utils;
 public class StringUtils {
 
 	public int FindNumberOfOccurences(String sentenceToScan, String characterToScanFor) {
-		try {
-			int numberOfOccurrences = 0;
-	
-			for (int i = 0; i < sentenceToScan.length(); i++) {
-				if (sentenceToScan.charAt(i) == characterToScanFor.charAt(0)) {
-					numberOfOccurrences++;
-				}
-			}
-			return numberOfOccurrences;
-		} catch (Exception e) {
+		if (characterToScanFor.length() != 1) {
 			throw new IllegalArgumentException();
+		}		
+		
+		int numberOfOccurrences = 0;
+
+		for (int i = 0; i < sentenceToScan.length(); i++) {
+			if (sentenceToScan.charAt(i) == characterToScanFor.charAt(0)) {
+				numberOfOccurrences++;
+			}
 		}
+		return numberOfOccurrences;
 	}
 }
